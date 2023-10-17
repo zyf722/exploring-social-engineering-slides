ef8a07f93948e40fb7a1f3411c63b737a242c6bc
zyf722 <1124982772@qq.com>
2023-10-17 16:26:13 +0800

    feat(layout): add 2x2 grid layout

---
+++ layouts/2x2-grid-header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout">
+    <div class="col-header mb-6" :class="props.layoutClass">
+      <slot />
+    </div>
+    <div class="two-columns w-full h-full grid grid-cols-2 grid-rows-2" :class="props.layoutClass">
+      <div class="col-top-left" :class="props.class">
+        <slot name="topleft" />
+      </div>
+      <div class="col-top-right" :class="props.class">
+        <slot name="topright" />
+      </div>
+      <div class="col-bottom-left" :class="props.class">
+        <slot name="bottomleft" />
+      </div>
+      <div class="col-bottom-right" :class="props.class">
+        <slot name="bottomright" />
+      </div>
+    </div>
+  </div>
+</template>
