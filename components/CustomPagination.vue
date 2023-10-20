6ad382b316bb44335c5166cf576cc470deea7ba3
zyf722 <1124982772@qq.com>
2023-10-20 11:04:52 +0800

    feat(pagination): add bottom-centered pagination

---
+++ components/CustomPagination.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="absolute text-xs bottom-0 right-0 w-full flex justify-center items-center">
+        <span><SlideCurrentNo /> / <SlidesTotal /></span>
+    </div>
+</template>
---
+++ global-top.vue
@@ -0,0 +1,6 @@
+<template>
+    <div v-if="$slidev.nav.currentLayout !== 'cover'">
+        <CustomPagination class="mb-1" />
+        <!-- <Footnotes separator class="mb-3" /> -->
+    </div>
+</template>
