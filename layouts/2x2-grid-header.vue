c3419fac2a4ff64e72307b3d159993b0c482ef8e
zyf722 <1124982772@qq.com>
2023-10-20 11:05:42 +0800

    feat(layout): update 2x2 grid layout

--- layouts/2x2-grid-header.vue
+++ layouts/2x2-grid-header.vue
@@ -12,20 +12,25 @@ const props = defineProps({
 <template>
   <div class="slidev-layout">
     <div class="col-header mb-6" :class="props.layoutClass">
-      <slot />
+        <slot />
     </div>
-    <div class="two-columns w-full h-full grid grid-cols-2 grid-rows-2" :class="props.layoutClass">
-      <div class="col-top-left" :class="props.class">
-        <slot name="topleft" />
+    <div class="two-columns w-full h-full grid grid-rows-2 gap-x-10" :class="props.layoutClass">
+      <div id="top" class="grid grid-cols-2">
+        <div class="col-top-left" :class="props.class">
+            <slot name="topleft" />
+        </div>
+        <div class="col-top-right" :class="props.class">
+            <slot name="topright" />
+        </div>
       </div>
-      <div class="col-top-right" :class="props.class">
-        <slot name="topright" />
-      </div>
-      <div class="col-bottom-left" :class="props.class">
-        <slot name="bottomleft" />
-      </div>
-      <div class="col-bottom-right" :class="props.class">
-        <slot name="bottomright" />
+
+      <div id="bottom" class="grid grid-cols-2 -mt-5">
+        <div class="col-bottom-left" :class="props.class">
+            <slot name="bottomleft" />
+        </div>
+        <div class="col-bottom-right" :class="props.class">
+            <slot name="bottomright" />
+        </div>
       </div>
     </div>
   </div>
