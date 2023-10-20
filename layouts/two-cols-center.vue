4b884f7ccc5825704722ccb61be656f00aa09f1c
zyf722 <1124982772@qq.com>
2023-10-20 17:08:35 +0800

    feat(layout): add two cols center layout

---
+++ layouts/two-cols-center.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout two-columns w-full h-full grid grid-cols-2" :class="props.layoutClass">
+    <div class="grid grid-cols-1 h-full place-content-center place-items-center my-auto" :class="props.class">
+        <slot />
+    </div>
+    <div class="grid grid-cols-1 h-full place-content-center place-items-center my-auto" :class="props.class">
+        <slot name="right" />
+    </div>
+  </div>
+</template>
